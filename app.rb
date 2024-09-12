78ae142079a6b5d1da0bcf31f0aea76b15028cf3
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-12 04:36:47 +0000

    Debug div form and results

--- app.rb
+++ app.rb
@@ -48,5 +48,9 @@ get("/divide") do
 end
 
 get("/wizard_divide") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num / @second_num
   erb(:div_result)
 end
