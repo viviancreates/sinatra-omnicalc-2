454d9d6a37302889e3866fd283eab74e03d78c58
viviancreates <168599473+viviancreates@users.noreply.github.com>
2024-09-12 04:26:08 +0000

    Debug mult form and result, fix subtraction bugs

--- app.rb
+++ app.rb
@@ -25,14 +25,21 @@ get("/wizard_subtract") do
   @first_num = params.fetch("first_num").to_f
   @second_num = params.fetch("second_num").to_f
 
+  @result = @first_num - @second_num
+
   erb(:sub_result)
 end
 
 get("/multiply") do
-  erb(:multiplication_form)
+  erb(:mult_form)
 end
 
-get("/wizard multiply") do
+get("/wizard_multiply") do
+  @first_num = params.fetch("first_num").to_f
+  @second_num = params.fetch("second_num").to_f
+
+  @result = @first_num * @second_num
+
   erb(:mult_result)
 end
 
